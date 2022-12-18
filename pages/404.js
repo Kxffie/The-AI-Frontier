a2b79d5966520f5c18903201c667113324cd5735
Kxffie <bryceriordan2005@gmail.com>
2022-12-18 13:12:04 -0600

    c

--- pages/404.js
+++ pages/404.js
@@ -11,10 +11,10 @@ export default function About() {
 			<div class="h-screen w-screen flex items-center">
 				<div class="container flex flex-col md:flex-row items-center justify-center text-black mx-auto">
 					<div class="max-w-md">
-						<div class="text-5xl font-dark font-bold text-violet-500">404</div>
+						<div class="text-5xl font-dark font-bold text-violet-500">HTTP 404</div>
 						<p class="text-2xl md:text-3xl font-light leading-normal">Sorry we couldn&apos;t find this page. </p>
 						<p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-						<Link href="/" class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-violet-500">back to homepage</Link>
+						<Link href="/" class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-violet-500">Back to Homepage</Link>
 					</div>
 				</div>
 			</div>
